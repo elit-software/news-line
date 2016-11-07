acea7b73689ad184448aa580ace1f7687af851dc
tiagomartinho <tiago.martinho@xpeppers.com>
2016-11-07 17:51:19 +0100

    action adds action to current

--- NewsLine/PassagePresenter.swift
+++ NewsLine/PassagePresenter.swift
@@ -1,12 +1,18 @@
 class PassagePresenter {
 
     let data: [PassageData]
+    var current: [PassageData]
 
     init(data: [PassageData]) {
         self.data = data
+        self.current = [data.first!]
     }
 
-    var current: [PassageData] {
-        return [data.first!]
+
+    func action(step: String) {
+        let findPassage = data.filter { (passage) -> Bool in
+            return passage.name == step
+        }.first!
+        current.append(findPassage)
     }
 }
--- NewsLineTests/PassagePresenterTests.swift
+++ NewsLineTests/PassagePresenterTests.swift
@@ -12,4 +12,20 @@ class PassagePresenterTests: XCTestCase {
 
         XCTAssertEqual(root, presenter.current)
     }
+
+    func testAfterActionShowsInitialPassageDataPlusActionData() {
+        let data = Parser(content: Reader(name: "test", type: "html", bundle: Bundle(for: PassagePresenterTests.self)).read()).parse()
+        let root = PassageData(name: "Step 1",
+                               content: "Double-click this passage to edit it.",
+                               next: ["Step 2", "Step 3"])
+        let child1 = PassageData(name: "Step 2",
+                                 content: "Double-click this passage to edit it.",
+                                 next: [String]())
+        let expected = [root, child1]
+        let presenter = PassagePresenter(data: data)
+
+        presenter.action(step: "Step 2")
+        
+        XCTAssertEqual(expected, presenter.current)
+    }
 }
