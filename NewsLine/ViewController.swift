b3df87a7a581e9ca9f64325aa88799fa27da5ef8
tiagomartinho <tiago.martinho@xpeppers.com>
2016-11-07 17:57:22 +0100

    view controller draws current content

--- NewsLine/ViewController.swift
+++ NewsLine/ViewController.swift
@@ -1,10 +1,24 @@
 import UIKit
 
-class ViewController: UIViewController {
+class TableViewController: UITableViewController {
 
     let data = Parser(content: Reader(name: "A").read()).parse()
 
+    var presenter: PassagePresenter!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "DefaultCell")
+        presenter = PassagePresenter(data: data)
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return presenter.current.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DefaultCell")!
+        cell.textLabel?.text = presenter.current[indexPath.row].content
+        return cell
     }
 }
